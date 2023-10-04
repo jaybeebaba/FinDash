367bf5ecc3937091446dcfbeab11184635ea8fe2
Jaybee <jubril_tunde@yahoo.com>
2023-10-04 20:44:21 +0100

    added checkbox to filter transactions by type

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import "./Profile.css"
+// Profile.js
+
+import React from 'react';
+import { UseAuthContext } from '../../hooks/UseAuthContext';
 
 const Profile = () => {
-  return (
-    <div style={{marginLeft: "300px"}}>Profile</div>
-  )
-}
+  const { user } = UseAuthContext();
 
-export default Profile
+  return (
+    <div className="profile wrapper">
+      <h2>Profile Page</h2>
+      {user && (
+        <div className="profile-info">
+          <img src={user.photoURL} alt="Profile" />
+          <h3>Name: {user.displayName}</h3>
+          <p>Email: {user.email}</p>
+          <p>UID: {user.uid}</p>
+          {/* Add more details if needed */}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
--- src/pages/transactions/Transactions.js
+++ src/pages/transactions/Transactions.js
@@ -1,47 +1,75 @@
-import {useState, useEffect} from 'react'
-import "../../components/recent/Recent.css"
-import {UseAuthContext} from "../../hooks/UseAuthContext"
+import { useState, useEffect } from 'react';
+import "../../components/recent/Recent.css";
+import { UseAuthContext } from "../../hooks/UseAuthContext";
+import "./Transactions.css"
 
 const Transactions = () => {
-  const {user} = UseAuthContext()
+  const { user } = UseAuthContext();
 
-  const [transactions, setTransactions] = useState(null)
+  const [transactions, setTransactions] = useState(null);
+  const [showIncomes, setShowIncomes] = useState(true); // Added state for showing incomes
+  const [showExpenses, setShowExpenses] = useState(true); // Added state for showing expenses
 
-  const transactionList = transactions ? (transactions.map(transaction =>{
-   
-      return(
+  const handleIncomesToggle = () => {
+    setShowIncomes(!showIncomes);
+  };
+
+  const handleExpensesToggle = () => {
+    setShowExpenses(!showExpenses);
+  };
+
+  useEffect(() => {
+    const current = JSON.parse(localStorage.getItem('transactions'));
+    const userTransaction = current.filter(transaction => transaction.createdBy.id === user.uid).sort((a, b) => Date.parse(b.date) - Date.parse(a.date));
+
+    setTransactions(userTransaction);
+  }, [user]);
+
+  const filteredTransactions = transactions ? transactions.filter(transaction => {
+    return (showIncomes && transaction.type === 'income') || (showExpenses && transaction.type === 'expense');
+  }) : [];
+
+  const transactionList = filteredTransactions.length > 0 ? (
+    filteredTransactions.map(transaction => {
+      return (
         <li key={transaction.id}>
-              <p>{transaction.name}</p>
-              <p>{transaction.type}</p>
-              <p>{transaction.category}</p>
-              <p>{transaction.date}</p>
-              <p>${transaction.amount}</p>
-            </li>
-      )
-    
-  })):(<>No Transactions</>)
-  useEffect(()=>{
-    const current = JSON.parse(localStorage.getItem('transactions'))
-    const userTransaction = current.filter(transaction => transaction.createdBy.id === user.uid).sort((a,b) =>   Date.parse(b.date) - Date.parse(a.date))
+          <p>{transaction.name}</p>
+          <p>{transaction.type}</p>
+          <p>{transaction.category}</p>
+          <p>{transaction.date}</p>
+          <p>${transaction.amount}</p>
+        </li>
+      );
+    })
+  ) : (
+    <li>No Transactions</li>
+  );
 
-    setTransactions(userTransaction)
- }, [])
- 
   return (
     <div className='transactions wrapper'>
-        <h4>Transactions</h4>
-        <ul>
-          <li>
-            <p>Name</p>
-            <p>Type</p>
-            <p>Category</p>
-            <p>Date</p>
-            <p>Amount($)</p>
-          </li>
-          {transactionList}
-        </ul>
+      <h4>Transactions</h4>
+      <div>
+        <label className='input'>
+          <p>Incomes</p>
+          <input type='checkbox' checked={showIncomes} onChange={handleIncomesToggle} />
+        </label>
+        <label className='input'>
+          <p>Expenses</p>
+          <input type='checkbox' checked={showExpenses} onChange={handleExpensesToggle} />
+        </label>
+      </div>
+      <ul>
+        <li>
+          <p>Name</p>
+          <p>Type</p>
+          <p>Category</p>
+          <p>Date</p>
+          <p>Amount($)</p>
+        </li>
+        {transactionList}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Transactions
+export default Transactions;
